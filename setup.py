fc49b015041e44da9482bd295eb8ed79cc7b6691
Gabriele Berton <s252805@studenti.polito.it>
2025-04-17 21:37:12 -0700

    Using pyproject.toml and better versioning

--- setup.py
+++ setup.py
@@ -7,16 +7,18 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = "1.0.5"
-DESCRIPTION = "A tool to download files. It supports progress bar, cksum, timeout, retry failed download."
+# Get version from __version__.py
+version = {}
+with open(os.path.join(here, "py3_wget", "__version__.py")) as f:
+    exec(f.read(), version)
 
 # Setting up
 setup(
     name="py3_wget",
-    version=VERSION,
+    version=version["__version__"],
     author="Gabriele Berton",
-    author_email="<berton.gabri@gmail.com>",
-    description=DESCRIPTION,
+    author_email="bertongabri@gmail.com",
+    description="A tool to download files. It supports progress bar, cksum, timeout, retry failed download.",
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
